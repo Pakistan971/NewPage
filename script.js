ef5136fd2ffa0a679bde37df800c702be409f480
Pakistan971 <161756431+Pakistan971@users.noreply.github.com>
2024-03-30 13:01:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openURL(Link) {
+    if(Link === 'JoinMe')
+    {
+        window.open('https://wa.me/+923458914120');
+    }
+    if(Link === 'YouTube')
+    {
+        window.open('https://www.linkedin.com/authwall?trk=bf&trkInfo=AQGvdLV2kp533wAAAY6OT32wOTa-WQkWyCebGOSA6pbFkhJynZHsR8hRdh3TZYlHWIjljX5fWkyOsGhh0EQjb-r8ISNhGrTxGZUGlOod6nfA16CdScoLsuS-ACVi1qGNRyZ0BnI=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fshahbaz-ali-5062bb239%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_app');
+    }
+    if(Link === 'Instagram')
+    {
+        window.open('https://www.instagram.com/black_vigo_971/');
+    }
+    if(Link === 'Whatsapp')
+    {
+        window.open('https://wa.me/+923458914120');
+    }
+    if(Link === 'GitHub')
+    {
+        window.open('https://github.com/Pakistan971');
+    }
+  }
